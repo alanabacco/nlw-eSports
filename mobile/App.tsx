e79acf9350b4c0f05378f12e0394288d68b2e9dc
Alana Bacco <alana_bacco@hotmail.com>
2022-10-15 14:54:03 -0300

    first commit

---
+++ mobile/App.tsx
@@ -0,0 +1,59 @@
+// LINHAS COMENTADAS TEM RELAÇÃO COM NOTIFICAÇÃO
+// TODO: Melhoria: implementar notificação no backend
+
+// import { useEffect, useRef } from "react";
+import { StatusBar } from "react-native";
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from "@expo-google-fonts/inter";
+// import { Subscription } from "expo-modules-core";
+// import * as Notifications from "expo-notifications";
+
+import { Routes } from "./src/routes";
+import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
+
+// import "./src/service/notificationConfigs";
+// import { getPushNotificationToken } from "./src/service/getPushNotificationToken";
+
+export default function App() {
+  // const getNotificationListener = useRef<Subscription>();
+  // const responseNotificationListener = useRef<Subscription>();
+
+  // useEffect(() => {
+  //   getPushNotificationToken();
+  // }, []);
+
+  // useEffect(() => {
+  //   getNotificationListener.current = Notifications.addNotificationReceivedListener(
+  //     (notification) => {
+  //       console.log(notification);
+  //     }
+  //   );
+
+  //   responseNotificationListener.current =
+  //     Notifications.addNotificationResponseReceivedListener((response) => {
+  //       console.log(response);
+  //     });
+
+  //   return () => {
+  //     if (getNotificationListener.current && responseNotificationListener.current) {
+  //       Notifications.removeNotificationSubscription(getNotificationListener.current);
+  //       Notifications.removeNotificationSubscription(responseNotificationListener.current);
+  //     }
+  //   };
+  // }, []);
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
+  return (
+    <Background>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </Background>
+  );
+}
